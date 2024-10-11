862c609dd24b59f4f4a5e2eacf2f158932b8e6e5
Joachim26 <Zangbeto229>
2024-10-11 17:51:53 +0200

    changement lien footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
       </ul>
 
       <div className="footer__socials">
-      <a href="https://www.linkedin.com/in/joachim-degboe-a26781254/" target="_blank"><BsLinkedin/></a>
+      <a href="https://www.linkedin.com/in/joachim-degboe/" target="_blank"><BsLinkedin/></a>
         <a href="https://github.com/JOKE26" target="_blank"><BsGithub/></a>
       </div>
 

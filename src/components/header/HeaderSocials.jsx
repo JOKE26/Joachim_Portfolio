a3da91a5e6c3665f438ab27e7b4e6338e435de6d
Joachim26 <Zangbeto229>
2024-10-18 02:00:31 +0200

    Changement lien linkedin header

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/joachim-degboe-a26781254/" target="_blank"><BsLinkedin/></a>
+        <a href="https://www.linkedin.com/in/joachim-degboe/" target="_blank"><BsLinkedin/></a>
         <a href="https://github.com/JOKE26" target="_blank"><BsGithub/></a>
     </div>
   )

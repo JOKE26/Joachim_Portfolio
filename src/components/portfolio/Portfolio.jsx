b1c8a4ea740a162a1308afcfbab636684722c86d
Joachim26 <Zangbeto229>
2025-01-27 14:03:25 +0100

    modification sur le portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,37 +6,37 @@ const data = [
   {
     id: 1,
     image: IMG,
-    title: "Angular_pokemon",
-    description: "Projet réalisé avec Angular et présent sur mon Github",
-    github: "https://github.com/JOKE26/Angular_pokemon",
-  },
-  {
-    id: 2,
-    image: IMG,
-    title: "Flutter_dart-flutter",
-    description: "Projet réalisé avec Flutter et présent sur mon Github",
-    github: "https://github.com/JOKE26/Flutter_dart-flutter",
-  },
-  {
-    id: 3,
-    image: IMG,
     title: "SAMBO NETWORK",
     description: "Site web réalisé avec Elementor sur WordPress",
     demo: "https://sambonetwork.fr/",
   },
   {
-    id: 4,
+    id: 2,
     image: IMG,
     title: "Pharmacie du Port de Pantin",
     description: "Site Web réalisé avec Elementor sur WordPress",
     demo: "https://pharmacieduportdepantin.com/",
   },
   {
-    id: 5,
+    id: 3,
     image: IMG,
     title: "La Plume Industry",
     description: "Site e-commerce réalisé grâce à Shopify",
     demo: "https://laplumeindustry.com/",
+  },
+  {
+    id: 4,
+    image: IMG,
+    title: "Angular_pokemon",
+    description: "Projet réalisé avec Angular et présent sur mon Github",
+    github: "https://github.com/JOKE26/Angular_pokemon",
+  },
+  {
+    id: 5,
+    image: IMG,
+    title: "Flutter_dart-flutter",
+    description: "Projet réalisé avec Flutter et présent sur mon Github",
+    github: "https://github.com/JOKE26/Flutter_dart-flutter",
   },
 ];
 
